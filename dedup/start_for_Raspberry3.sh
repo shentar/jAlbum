f9dcc6b1ec54986c2af5e31875b1c4163d1d998e
root <tyanqun@mail.com>
2016-06-19 14:35:26 +0800

    change dirs

--- dedup/start_for_Raspberry3.sh
+++ dedup/start_for_Raspberry3.sh
@@ -2,4 +2,4 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd )"
 cd "${DIR}"
-(java -Xdebug -Xrunjdwp:transport=dt_socket,address=4321,server=y,suspend=n -Dorg.sqlite.lib.path=../jdbcsql/ -Dorg.sqlite.lib.name=libsqlite.so -Dinputdir=/media/Ent -Dthreadcount=4 -Dhashalog=MD5 -jar start.jar >./log/jstdout.log 2>&1  &)
+(java -Xdebug -Xrunjdwp:transport=dt_socket,address=4321,server=y,suspend=n -Dorg.sqlite.lib.path=../jdbcsqlitenative/ -Dorg.sqlite.lib.name=libsqlite.so -Dinputdir=/media/Ent -Dthreadcount=4 -Dhashalog=MD5 -jar start.jar >./log/jstdout.log 2>&1  &)
