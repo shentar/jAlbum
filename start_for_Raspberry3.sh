5770d7cfeb679b655ab689de8d2a2e6928aa7f09
root <tyanqun@gmail.com>
2016-06-26 22:26:48 +0800

    fix bug

--- start_for_Raspberry3.sh
+++ start_for_Raspberry3.sh
@@ -2,4 +2,4 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd )"
 cd "${DIR}/dedup"
-(java -Xdebug -Xrunjdwp:transport=dt_socket,address=4321,server=y,suspend=n -Dorg.sqlite.lib.path=../jdbcsqlitenative/ -jar start.jar >./log/jstdout.log 2>&1  &)
+(java -Xdebug -Xrunjdwp:transport=dt_socket,address=4321,server=y,suspend=n -Dorg.sqlite.lib.path=../jdbcsqlitenative/ -Dorg.sqlite.lib.name=libsqlite.so -jar start.jar >./log/jstdout.log 2>&1  &)
