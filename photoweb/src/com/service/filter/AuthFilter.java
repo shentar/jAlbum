f9e2c8e26374fe4e5725bdbf3163cda3d705e6f7
shentar <tyanqun@mail.com>
2017-05-15 23:04:31 +0800

    bug fix.

--- photoweb/src/com/service/filter/AuthFilter.java
+++ photoweb/src/com/service/filter/AuthFilter.java
@@ -109,7 +109,7 @@ public class AuthFilter extends AbstractFilter
         httpres.setStatus(200);
         try
         {
-            String origUri = httpreq.getParameter("origurl");
+            String origUri = httpreq.getParameter(ORIGNAL_URI_KEY);
             String hh = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" "
                     + "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"
                     + "<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"zh-CN\">"
