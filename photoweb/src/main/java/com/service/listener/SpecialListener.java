acf9c29767064d041b08ae7d5d066b5c5c26d2ee
Shentar <tyanqun@mail.com>
2018-11-17 21:00:15 +0800

    add auto backup task everyday.

--- photoweb/src/main/java/com/service/listener/SpecialListener.java
+++ photoweb/src/main/java/com/service/listener/SpecialListener.java
@@ -1,5 +1,6 @@
 package com.service.listener;
 
+import java.util.Calendar;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -20,7 +21,11 @@ public class SpecialListener implements ServletContextListener
 {
     private static Logger logger = LoggerFactory.getLogger(SpecialListener.class);
 
-    private static Future<?> f = null;
+    private static Future<?> fFreshAllData = null;
+
+    private static Future<?> fBackupScanTask = null;
+
+    private static final int HOUR_TO_BACKUP = 22;
 
     static
     {
@@ -32,7 +37,8 @@ public class SpecialListener implements ServletContextListener
     public void contextDestroyed(ServletContextEvent arg0)
     {
         logger.warn("began to stop the web service.");
-        f.cancel(true);
+        fFreshAllData.cancel(true);
+        fBackupScanTask.cancel(true);
         FileTools.threadPool.shutdownNow();
         FileTools.threadPool4Thumbnail.shutdownNow();
         logger.warn("stopped the web service");
@@ -47,10 +53,21 @@ public class SpecialListener implements ServletContextListener
 
     private void startBackGroundTask()
     {
-        BackendScanner.getInstance().scheduleOneTask();
+        // 每天备份数据到远端云存储。
+        fBackupScanTask = new ScheduledThreadPoolExecutor(1).scheduleWithFixedDelay(new Runnable()
+        {
+            public void run()
+            {
+                if (Calendar.getInstance().get(Calendar.HOUR_OF_DAY) == HOUR_TO_BACKUP)
+                {
+                    BackendScanner.getInstance().scheduleOneTask();
+                }
+            }
+        }, 10, 5 * 60, TimeUnit.SECONDS);
+
 
         // 5秒检查是否需要刷新数据表。
-        f = new ScheduledThreadPoolExecutor(1).scheduleWithFixedDelay(new Runnable()
+        fFreshAllData = new ScheduledThreadPoolExecutor(1).scheduleWithFixedDelay(new Runnable()
         {
             public void run()
             {
