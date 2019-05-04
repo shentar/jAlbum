9af290f402d68abf461abf0a19dab1e99872f0ae
Shentar <tyanqun@gmail.com>
2019-05-04 14:30:17 +0800

    更新 'scripts/start.sh'

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 cmd="${1}"
 [ -z "${cmd}" ] && exit 1
 
@@ -13,66 +12,64 @@ pid=`cat ${pidfile}` 2>/dev/null
 
 running()
 {
-	 [ -z "${pid}" ] && return 1
-	 kill -0 "${pid}" 2>/dev/null
+    [ -z "${pid}" ] && return 1
+    kill -0 "${pid}" 2>/dev/null
 }
 
 startup()
 {
-		running
-        if [ $? -eq 0 ]
-	    then 
-		    echo "already running"
-		else
-            java -Xms512M -Xmx512M -Xdebug -Xrunjdwp:transport=dt_socket,address=4321,server=y,suspend=n -jar start.jar > log/jstdout.txt 2>&1 &
-            ret=$?
-            echo $! > "${pidfile}"
-		    if [ ${ret} -ne 0 ]
-		    then
-			    echo "start failed"
-		    else
-			    echo "started"	    
-            fi	     
-		fi 	
+    running
+    if [ $? -eq 0 ]
+    then 
+        echo "already running"
+    else
+        java -Xms512M -Xmx512M -Xdebug -Xrunjdwp:transport=dt_socket,address=4321,server=y,suspend=n -jar start.jar > log/jstdout.txt 2>&1 &
+        ret=$?
+        disown $!
+        echo $! > "${pidfile}"
+        if [ ${ret} -ne 0 ]
+        then
+            echo "start failed"
+        else
+            echo "started"        
+        fi         
+    fi     
 }
 
 stopnow()
 {
-		running
-		if [ $? -eq 0 ]
-		then
-			kill -9 ${pid}
-            rm -f "${pidfile}" >/dev/null 2>&1
-            echo "stopped"
-		else
-			echo "not running"
-		fi
+    running
+    if [ $? -eq 0 ]
+    then
+        kill -9 ${pid}
+        rm -f "${pidfile}" >/dev/null 2>&1
+        echo "stopped"
+    else
+        echo "not running"
+    fi
 }
 
-
-
 case ${cmd} in
-	status)
-		running
-		if [ $? -eq 0 ]
-		then 
-			echo "running"
-		else
-			echo "not running"
+    status)
+        running
+        if [ $? -eq 0 ]
+        then 
+            echo "running"
+        else
+            echo "not running"
             rm -f "${pidfile}" >/dev/null 2>&1
-		fi
-		;;
-	start)
+        fi
+        ;;
+    start)
         startup
-		;;	
-	stop)
+        ;;    
+    stop)
         stopnow
-		;;
-	restart)
-		stopnow
-		startup
-		;;
+        ;;
+    restart)
+        stopnow
+        startup
+        ;;
 esac
-
 
 exit 0
