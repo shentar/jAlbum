d1b0993ea25f3f3bdf5295e1d801cfe6933669d7
Tong Yanqun <tyanqun@gmail.com>
2023-03-06 17:49:45 +0800

    matomo site id.

--- photoweb/src/main/java/com/utils/web/GenerateHTML.java
+++ photoweb/src/main/java/com/utils/web/GenerateHTML.java
@@ -1104,7 +1104,7 @@ public class GenerateHTML {
             return "<script> var _paq = window._paq = window._paq || [];" +
                     "_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);" +
                     "(function() {var u=\"//analytics.codefine.site:6870/\";" +
-                    "_paq.push(['setTrackerUrl', u+'matomo.php']); _paq.push(['setSiteId', '2']);" +
+                    "_paq.push(['setTrackerUrl', u+'matomo.php']); _paq.push(['setSiteId', '5']);" +
                     "var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];" +
                     "g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);})();</script>";
         } else {
